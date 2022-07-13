a974b259a3113498583968e72c24605cd7b800e8
EliasCassimiro <eliascassimiro1948@outlook.com>
2022-07-13 10:59:21 -0300

    criando arquivo config

---
+++ src/config/config.php
@@ -0,0 +1,8 @@
+<?php
+    date_default_timezone_set('America/Sao_Paulo');
+    setlocale(LC_TIME, 'pt-BR', 'pt-BR.uft-8', 'portuguese');
+
+
+    //Pastas
+    define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
+?>
