364e4febcae33f032cd774583da9be2c3b1f7c6f
EliasCassimiro <eliascassimiro1948@outlook.com>
2022-07-13 12:03:51 -0300

    arquivo atualizado

--- src/config/config.php
+++ src/config/config.php
@@ -5,4 +5,6 @@
 
     //Pastas
     define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
+
+    require_once(realpath(dirname(__FILE__) . '/database.php'));
 ?>
---
+++ src/models/Model.php
@@ -0,0 +1,29 @@
+<?php
+    class Model{
+        protected static $tableName = '';
+        protected static $columns = [];
+        protected $values = [];
+
+        function __construct($array){
+           $this -> loadFromArray($array);
+        }
+
+        public function loadFromArray($array){
+            if($array){
+                foreach($array as $key => $value){
+                    $this -> $key = $value;
+                }
+            }
+        }
+
+        public function __get($key){
+            return $this -> values[$key];
+        }
+
+        public function __set($key, $value){
+            $this -> values[$key] = $value;
+        }
+    }
+
+
+?>
