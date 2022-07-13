f45d98963b1a8bb79bfeda984018846d6346f9bc
EliasCassimiro <eliascassimiro1948@outlook.com>
2022-07-13 15:44:13 -0300

    obtendo usuarios do banco

--- public/index.php
+++ public/index.php
@@ -2,9 +2,8 @@
     require_once(dirname(__FILE__, 2) . '/src/config/config.php');
     require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-    $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3er.com.br']);
-    print_r($user);
-    echo '<br>';
-    $user -> email = 'lucas_alterado@cod3r.com.br';
-    print_r($user -> email);
+    foreach(User::get([], 'name') as $user){
+        echo $user -> name;
+        echo '<br>';
+    }
 ?>
