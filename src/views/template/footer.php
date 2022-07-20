7e4136d98022a70c823b6a4b9b604b1051a608f6
EliasCassimiro <eliascassimiro1948@outlook.com>
2022-07-20 14:30:52 -0300

    criando app.js

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -3,5 +3,6 @@
             <span><i class="icofont-heart text-danger mx-1"></i></span>
             <span>por Elias Cassimiro</span>
         </footer>
+        <script src="assets/css/js/app.js"></script>
     </body>
 </html>
