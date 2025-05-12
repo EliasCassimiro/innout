ab64fcf11dba36f2f206d9cefcd41dbbf002df47
Elias Cassimiro <eliascassimiro1948@outlook.com>
2025-05-12 10:55:13 -0300

    aula

--- public/index.php
+++ public/index.php
@@ -1,12 +1,6 @@
 <?php
     require_once (dirname(__FILE__, 2) . '/src/config/config.php');
-    require_once (dirname(__FILE__, 2) . '/src/models/User.php');
+    require_once (dirname(__FILE__, 2) . '/src/views/Login.php');
 
-    $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-    print_r(User::get(['id' => 1], 'name, email'));
-    echo '<br>';
-    print_r(User::get(['name' => 'Chaves'], 'name, email'));
-    echo '<br>';
-    print_r(User::get());
 ?>
 
