ab1314169a5eb503f601ac8fc8ecc427025ddf48
Elias Cassimiro <eliascassimiro1948@outlook.com>
2025-10-14 10:01:54 -0300

    aula

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     const menuToggle = document.querySelector('.menu-toggle')
     menuToggle.onclick = function (e) {
         const body = document.querySelector('body')
@@ -6,24 +6,25 @@
     }
 })()
 
-function addOneSecond(hours, minutes, seconds) {
-    const d = new Date()
-    d.setHours(parseInt(hours))
-    d.setMinutes(parseInt(minutes))
-    d.setSeconds(parseInt(seconds) + 1)
-
-    const h = `${d.getHours()}`.padStart(2,0)
-    const m = `${d.getMinutes()}`.padStart(2,0)
-    const s = `${d.getSeconds()}`.padStart(2,0)
-
-    return `${h}:${m}:${s}`
-}
-
 function activateClock() {
     const activeClock = document.querySelector('[active-clock]')
     if(!activeClock) return
 
-    setInterval(function(){
+    function addOneSecond(hours, minutes, seconds) {
+        const d = new Date()
+        d.setHours(parseInt(hours))
+        d.setMinutes(parseInt(minutes))
+        d.setSeconds(parseInt(seconds) + 1)
+    
+        const h = `${d.getHours()}`.padStart(2, 0)
+        const m = `${d.getMinutes()}`.padStart(2, 0)
+        const s = `${d.getSeconds()}`.padStart(2, 0)
+    
+        return `${h}:${m}:${s}`
+    }
+
+    setInterval(function() {
+        // '07:27:19' => ['07', '27', '19']
         const parts = activeClock.innerHTML.split(':')
         activeClock.innerHTML = addOneSecond(...parts)
     }, 1000)
