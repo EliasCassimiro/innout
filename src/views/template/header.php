ac10b453f60a0cb3974ea0df25576cf3be58f582
Elias Cassimiro <eliascassimiro1948@outlook.com>
2025-10-14 10:42:37 -0300

    correções

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -33,7 +33,7 @@
                     <span class="ml-3"><?= $_SESSION['user']->name ?></span>
                     <i class="icofont-simple-down mx-2"></i>
                 </div>
-                
+
                 <div class="dropdown-content">
                     <ul class="nav-list">
                         <li class="nav-item">
